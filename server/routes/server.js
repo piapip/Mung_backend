c4cc252dcdfa8adda936efc98265cbc80572a48e
piapip <thovinh9997@gmail.com>
2021-05-29 13:26:47 +0700

    Disable unused API in server.js.

--- server/routes/server.js
+++ server/routes/server.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { User } = require("../models/User");
 // const { Chatroom } = require("../models/Chatroom");
 // const { Audio } = require("../models/Audio");
-const axios = require("axios");
-const fs = require("fs");
+// const axios = require("axios");
+// const fs = require("fs");
 // const intentSamplePool = require("./../config/intent");
 
 // count user
@@ -121,67 +121,67 @@ router.get("/user", (req, res) => {
 //   });
 // });
 
-router.get("/flatten", (req, res) => {
-  Chatroom.findOne()
-    .skip(1)
-    .populate("currentIntent")
-    .exec((err, roomFound) => {
-      if (err) res.status(500).send({ success: false, err });
-      const result = flattenIntent(roomFound.currentIntent);
-      return res.status(200).send({
-        success: true,
-        result,
-      });
-    });
-});
+// router.get("/flatten", (req, res) => {
+//   Chatroom.findOne()
+//     .skip(1)
+//     .populate("currentIntent")
+//     .exec((err, roomFound) => {
+//       if (err) res.status(500).send({ success: false, err });
+//       const result = flattenIntent(roomFound.currentIntent);
+//       return res.status(200).send({
+//         success: true,
+//         result,
+//       });
+//     });
+// });
 
-router.get("/export-user", async (req, res) => {
-  const { destination, name } = req.body;
+// router.get("/export-user", async (req, res) => {
+//   const { destination, name } = req.body;
 
-  await User.find()
-    .then(async (userFound) => {
-      await exportObject(
-        `${path.join(process.cwd(), '..', destination, name + ".json")}`,
-        userFound,
-        () => {
-          let formData = new FormData();
-          formData.append("destination", destination);
-          formData.append("name", name);
-          formData.append(
-            "file",
-            fs.createReadStream(
-              path.join(process.cwd(), '..', destination, name + ".json")
-            )
-          );
+//   await User.find()
+//     .then(async (userFound) => {
+//       await exportObject(
+//         `${path.join(process.cwd(), '..', destination, name + ".json")}`,
+//         userFound,
+//         () => {
+//           let formData = new FormData();
+//           formData.append("destination", destination);
+//           formData.append("name", name);
+//           formData.append(
+//             "file",
+//             fs.createReadStream(
+//               path.join(process.cwd(), '..', destination, name + ".json")
+//             )
+//           );
 
-          axios({
-            method: "POST",
-            url: `https://asr.vbeecore.com/api/v1/uploads/file`,
-            data: formData,
-            headers: {
-              "Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}`,
-              Authorization: `Bearer zyvZQGPrr6qdbHLTuzqpCmuBgW3TjTxGKEEIFCiy1lCAOzTBtrqPYdPdZ1AtMxU2`,
-            },
-            maxContentLength: "Infinity",
-            maxBodyLength: "Infinity",
-          })
-            .then((response) => {
-              res.status(200).send(response.data);
-            })
-            .catch((error) => res.status(500).send(error));
-        }
-      );
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .send("Internal problem... Can't get User's information. Err:");
-      throw err;
-    });
-});
+//           axios({
+//             method: "POST",
+//             url: `https://asr.vbeecore.com/api/v1/uploads/file`,
+//             data: formData,
+//             headers: {
+//               "Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}`,
+//               Authorization: `Bearer zyvZQGPrr6qdbHLTuzqpCmuBgW3TjTxGKEEIFCiy1lCAOzTBtrqPYdPdZ1AtMxU2`,
+//             },
+//             maxContentLength: "Infinity",
+//             maxBodyLength: "Infinity",
+//           })
+//             .then((response) => {
+//               res.status(200).send(response.data);
+//             })
+//             .catch((error) => res.status(500).send(error));
+//         }
+//       );
+//     })
+//     .catch((err) => {
+//       res
+//         .status(500)
+//         .send("Internal problem... Can't get User's information. Err:");
+//       throw err;
+//     });
+// });
 
-const FormData = require("form-data");
-const path = require("path");
+// const FormData = require("form-data");
+// const path = require("path");
 
 // count main intent for rooms
 // router.get("/export-conversation", async (req, res) => {
@@ -312,37 +312,37 @@ router.get("/test", (req, res) => {
   res.status(200).send("ok");
 });
 
-const exportObject = (destination, object, callback) => {
-  console.log("Destination: ", destination);
-  fs.writeFile(destination, JSON.stringify(object), (err) => {
-    // return doesn't work...
-    if (err) {
-      console.log(err);
-    }
-    if (callback) {
-      callback();
-    }
-  });
-};
+// const exportObject = (destination, object, callback) => {
+//   console.log("Destination: ", destination);
+//   fs.writeFile(destination, JSON.stringify(object), (err) => {
+//     // return doesn't work...
+//     if (err) {
+//       console.log(err);
+//     }
+//     if (callback) {
+//       callback();
+//     }
+//   });
+// };
 
-const flattenIntent = (currentIntent) => {
-  const {
-    intent,
-    loan_purpose,
-    loan_type,
-    card_type,
-    card_usage,
-    digital_bank,
-    card_activation_type,
-    district,
-    city,
-    name,
-    cmnd,
-    four_last_digits,
-    generic_intent,
-  } = currentIntent;
-  return `${intent}_${loan_purpose}_${loan_type}_${card_type}_${card_usage}_${digital_bank}_${card_activation_type}_${district}_${city}_${name}_${cmnd}_${four_last_digits}_${generic_intent}`;
-};
+// const flattenIntent = (currentIntent) => {
+//   const {
+//     intent,
+//     loan_purpose,
+//     loan_type,
+//     card_type,
+//     card_usage,
+//     digital_bank,
+//     card_activation_type,
+//     district,
+//     city,
+//     name,
+//     cmnd,
+//     four_last_digits,
+//     generic_intent,
+//   } = currentIntent;
+//   return `${intent}_${loan_purpose}_${loan_type}_${card_type}_${card_usage}_${digital_bank}_${card_activation_type}_${district}_${city}_${name}_${cmnd}_${four_last_digits}_${generic_intent}`;
+// };
 
 // const getLabel = (slot) => {
 //   const slotIndex = intentSamplePool.SLOT_LABEL.findIndex((item) => {
