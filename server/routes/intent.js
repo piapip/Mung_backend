d226a40befe28fdb2b44fb89a635c562052c60f8
piapip <thovinh9997@gmail.com>
2021-06-12 10:18:34 +0700

    Update question pool.

--- server/routes/intent.js
+++ server/routes/intent.js
@@ -9,7 +9,7 @@ router.get("/random", (req, res) => {
   fs.readFile(path.join(process.cwd(), "server", "config", "intent_v2.json"), (err, data) => {
     if (err) throw err;
     let intentList = JSON.parse(data);
-    const intentIndex = getRandomFromArray(intentList.slice(0, 20));
+    const intentIndex = getRandomFromArray(intentList.slice(0, 40));
     const { intent, question } = intentList[intentIndex]
     res.status(200).send({
       intent,
